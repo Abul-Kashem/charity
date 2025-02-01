e74ed3f17f67de02e9cc1be2efc3bfb3fa176ada
Abul-Kashem <kashem.mcpd@gmail.com>
2025-02-01 08:47:08 +0600

    causes slider work done

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,4 +17,15 @@
   $("[data-border-color]").each(function () {
     $(this).css("border-color", $(this).attr("data-border-color"));
   });
+
+  // swiper slider
+
+     var swiper = new Swiper(".ahncauses-slider-active", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
 })(jQuery);
